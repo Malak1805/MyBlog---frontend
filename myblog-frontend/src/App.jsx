5b7be35f66de717c6293642a457c840d561daf9d
Malak1805 <mjma200018@gmail.com>
2025-09-09 19:20:02 +0300

    restructured files + frontend and backend are connnected

--- myblog-frontend/src/App.jsx
+++ myblog-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-
+import Register from './components/pages/Register'
+import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
 
@@ -7,9 +8,9 @@ function App() {
  
 
   return (
-    <>
-  
-    </>
+    <Routes>
+  <Route path="/auth/register" element={<Register />} />
+    </Routes>
   )
 }
 
